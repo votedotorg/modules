047fafddda0f205a7c1e000bba401afc2f051426
Devorah Langsam <devorah@langsam.org>
2019-09-13 17:07:02 -0400

    they changed it back to type after i asked

--- packages/helpusersvote-react/src/components/polling-place-finder/utils/normalize.js
+++ packages/helpusersvote-react/src/components/polling-place-finder/utils/normalize.js
@@ -66,7 +66,7 @@ export function normalizeVoterInfo(info = {}) {
   // If there are contests, split them into general and referendums.
   if (info.contests) {
     data.generals = info.contests
-      .filter(c => c.contest == 'General')
+      .filter(c => c.type == 'General')
       .map(normalizeGeneral)
 
     data.referendums = info.contests
