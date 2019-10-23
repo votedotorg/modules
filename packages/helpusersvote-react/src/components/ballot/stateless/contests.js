932c00da6c80adb45b4ce440125f5fa77ac3d8a7
Devorah Langsam <devorah@langsam.org>
2019-10-23 09:53:29 -0400

    show a button to visit the candidate website when the api provides it

--- packages/helpusersvote-react/src/components/ballot/stateless/contests.js
+++ packages/helpusersvote-react/src/components/ballot/stateless/contests.js
@@ -110,6 +110,19 @@ function CandidateDetail({
 }) {
   const isChecked = ballot[contest.office] === candidate.key
   const isOtherChecked = ballot[contest.office] && !isChecked
+  let moreInfo
+  if (candidate.url != null) {
+      moreInfo = <a
+        className="huv-button fw5 fr relative"
+        href={candidate.url} 
+        target="_blank"
+        rel="noopener"
+      >
+        <span className="dn di-ns">Visit candidate's site</span> &rarr;
+      </a>;
+    } else {
+      moreInfo = '';
+    }
 
   return (
     <div
@@ -149,6 +162,7 @@ function CandidateDetail({
           <CandidateParties parties={candidate.parties} />
         </label>
       </div>
+      {moreInfo}
     </div>
   )
 }
